5918a5183bd8f415d74f4841cda8f3907871cbf1
Zain Ali <z.a.cheema7777@gmail.com>
2020-11-11 21:03:34 +0500

    created TasksAdapter

---
+++ app/src/main/java/com/thetechannel/android/planit/tasks/TasksAdapter.kt
@@ -0,0 +1,50 @@
+package com.thetechannel.android.planit.tasks
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.thetechannel.android.planit.data.source.domain.Task
+import com.thetechannel.android.planit.databinding.TaskItemBinding
+
+class TasksAdapter(private val viewModel: TasksViewModel) :
+    ListAdapter<Task, TasksAdapter.ViewHolder>(TaskDiffCallback()) {
+
+    class ViewHolder private constructor(val binding: TaskItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(viewModel: TasksViewModel, item: Task) {
+            binding.viewmodel = viewModel
+            binding.task = item
+            binding.executePendingBindings()
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = TaskItemBinding.inflate(layoutInflater, parent, false)
+
+                return ViewHolder(binding)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(viewModel, getItem(position))
+    }
+}
+
+class TaskDiffCallback : DiffUtil.ItemCallback<Task>() {
+    override fun areItemsTheSame(oldItem: Task, newItem: Task): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: Task, newItem: Task): Boolean {
+        return oldItem == newItem
+    }
+}
